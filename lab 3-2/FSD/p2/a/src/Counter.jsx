a7a9fd7d1c08693057083ab8ac1949cc6e73d091
sai vamsi <saivamsi5764@gamil.com>
2026-02-10 09:52:49 +0530

    DL, FSD

---
+++ lab 3-2/FSD/p2/a/src/Counter.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = { count: 0 };
+  }
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
+      <div>
+        <h2>Counter: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
